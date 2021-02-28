92103a2778a71a83042948325d9157de0d8b4f99
MatheusXavier15 <matheusxavier055@gmail.com>
2021-02-28 14:32:22 -0300

    Código comentado

--- app.js
+++ app.js
@@ -1,54 +1,68 @@
 new Vue({
   el: "#app",
   data: {
-    running: false,
-    playerLife: 100,
-    monsterLife: 100,
-    logs: []
+    running: false, //determina se o jogo ta ou não rodando
+    playerLife: 100, //vida do jogador 
+    monsterLife: 100, //vida do monstro 
+    logs: [] //array contendo os registros de jogadas
   },
   computed: {
-    hasResult() {
+    hasResult() { //método que verifica continuamente se algum dos jogadores perdeu, retornando o vencedor
       return this.playerLife == 0 || this.monsterLife == 0;
     },
   },
   methods: {
+
+    // método que inicia/refresh o jogo
     startGame() {
       this.running = true;
       this.playerLife = 100;
       this.monsterLife = 100;
       this.logs = []
     },
+
+    //método que retorna uma valor randomico dentro do interfalo passado  
     getRandom(min, max) {
       const value = Math.random() * (max - min) + min;
       return Math.round(value);
     },
+
+    //método que faz a cotação do ataque (machuca o monstro ou jogador)
     hurt(atr, min, max, especial, source, target, cls) {
       const plus = especial ? 5 : 0;
       const hurt = this.getRandom(min + plus, max + plus)
       this[atr] = Math.max(this[atr] - hurt, 0)
       this.registerLog(`${source} atingiu ${target} com ${hurt}.`,cls)
     },
-    attack(especial) {
+
+    //método do ataque
+    attack(especial) {//recebe o parâmetro especial como true ou false dependendo se o ataque é especial ou não
       this.hurt('playerLife',7,12,false, 'Monstro', 'Player', 'monster');
       if(this.monsterLife>0)
         this.hurt('monsterLife',5,10,especial, 'Jogador','Monstro', 'player');
     },
+
+    //método de cura
     heal(min,max){
         const heal = this.getRandom(min, max)
         this.playerLife= Math.min(this.playerLife + heal, 100)
         this.registerLog(`Jogador ganhou vida de ${heal}`,'player')
     },
+
+    //método que cura e causa dano
     healAndHurt(){
         this.heal(10,15)
         this.hurt('playerLife', 7,12, false, 'Monstro', 'Jogador', 'monster')
     },
+
+    //método que regista os logs
     registerLog(text, cls){
         this.logs.unshift({ text , cls })
     }
 
   },
   watch: {
-      hasResult(value){
+      hasResult(value){//quando ouver um resultado, seta o running para false
           if(value) this.running = false
       }
   },
